16390be4f66d9a81fc8bdad6a270bc0ec395f8c4
Sean Lauritzen <seanl@desktop.lauritzenwifi.net>
2025-01-14 11:07:44 -0500

    Old file cleanup

--- urcl/newparser/__init__.py
+++
@@ -1,4 +0,0 @@
-from lex import tokenize
-from cst import create_program_cst
-
-__all__ = ["tokenize", "create_program_cst"]
--- urcl/newparser/__main__.py
+++
@@ -1,8 +0,0 @@
-import __init__
-import sys
-
-from lex import TokenStream
-if __name__ == "__main__":
-    toks = __init__.tokenize(sys.stdin.read())
-    if isinstance(toks, TokenStream):
-        print(__init__.create_program_cst(toks))
